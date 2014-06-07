fb61e430065471da287c834f14b35295c59a2ecf
Michal Cywinski <mcywinski@hotmail.com>
2014-06-07 23:26:32 +0200

    Szkielet forkow

--- main.c
+++ main.c
@@ -2,6 +2,47 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+char[128] buffer;
+
+void read_data()
+{
+
+}
+
+void process_data()
+{
+}
+
+void output_data()
+{
+}
+
 int main()
 {
+	int p_id = fork();
+	if (p_id == -1) //Shit happens
+	{
+		printf("An error has occured while creating first derived process!\n");
+	}
+	else if (p_id == 0) //OK -> Derived process
+	{
+		int dp_id = fork(); //dp = derived process
+		if (dp_id == -1) //oh boy!
+		{
+			printf("An error has occured while creating second derived process!\n");
+		}
+		else if (dp_id == 0) //OK -> derived process of derived process (P3)
+		{
+			output_data();
+		}
+		else //OK -> derived process (P2)
+		{
+			process_data();
+		}
+	}
+	else //OK -> Parent process (P1)
+	{
+		read_data();
+	}
+	return 0;
 }
