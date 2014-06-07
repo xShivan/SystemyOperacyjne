36488f77951de0fac9a8311e2f244d5db993b773
Michal Cywinski <mcywinski@hotmail.com>
2014-06-08 01:15:15 +0200

    Wykonana 1-wsza czesc zadania

--- main.c
+++ main.c
@@ -1,26 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-
-char[128] buffer;
-
+#define SIZE 128
+short ok = 1;
+ 
+//String buffer
+char buffer[SIZE];
+ 
+//Data flow control
+int fd_rp[2]; //read-proc
+int fd_po[2]; //proc-out
+ 
 void read_data()
 {
-
+	int tmp;
+	close(fd_rp[0]);
+	while (ok)
+	{
+		if ((tmp = read(STDIN_FILENO, buffer, SIZE)) > 0)
+		{
+			write(fd_rp[1], buffer, tmp);
+		}
+	}
 }
-
+ 
 void process_data()
 {
+	int count = 0 , tmp, i;
+	close(fd_rp[1]);
+	close(fd_po[0]);
+	while (ok)
+	{
+		if((tmp = read(fd_rp[0], buffer, SIZE)) > 0)
+		{		   
+			for(i = 0; i < tmp; i++)
+			{
+				if(buffer[i] == '\n')
+				{
+					write(fd_po[1], &count, sizeof(count));
+					count = 0;
+				}
+				else count++;
+			}		
+		}
+	}
 }
-
+ 
 void output_data()
 {
+	int count = 0;
+	close(fd_po[1]);
+	while (ok)
+	{
+		if(read(fd_po[0], &count, sizeof(count)) > 0)
+		{
+			printf("Received %d characters...\n", count);
+		}
+	}
 }
-
+ 
 int main()
 {
+	   
+	pipe(fd_rp);
+	pipe(fd_po);
+   
 	int p_id = fork();
-	if (p_id == -1) //Shit happens
+	if (p_id == -1) //AWW! Shit happens
 	{
 		printf("An error has occured while creating first derived process!\n");
 	}
@@ -46,3 +93,4 @@ int main()
 	}
 	return 0;
 }
+
